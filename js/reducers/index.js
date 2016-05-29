5498650ef84362345fcce42bd8cb5a2685bfdd9f
Yishan Wen <wittywys@gmaill.com>
2016-05-29 21:18:59 +0800

    combineReducer helper implement

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -2,7 +2,15 @@ import { combineReducers } from 'redux'
 import userReducer from './userReducer'
 import filterReducer from './filterReducer'
 
-export default combineReducers({
-  userList: userReducer,
-  filter: filterReducer
-})
+// export default combineReducers({
+//   userList: userReducer,
+//   filter: filterReducer
+// })
+
+
+export default function (state = {}, action) {
+  return {
+    userList: userReducer(state.userList, action),
+    filter: filterReducer(state.filter, action)
+  }
+}
